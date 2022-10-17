0aec8139652f8cc4afc54025f21fe2e85664d5c4
Adrijan Parmakovski <adrijan.parmakovski@gmail.com>
2022-10-17 22:03:00 +0200

    add settings.file

---
+++ app/settings.py
@@ -0,0 +1,16 @@
+from dataclasses import dataclass
+from base64 import b64encode
+import os
+
+
+@dataclass(frozen=True)
+class SpotifyConfigs:
+    ACCOUNT_ID = os.environ['ACCOUNT_ID'].astype(str)
+    CLIENT_ID = os.environ['CLIENT_ID']
+    CLIENT_SECRET = os.environ['CLIENT_SECRET']
+    SCOPES = 'user-library-read user-library-modify'
+    REDIRECT_URI = 'http://google.com/'
+    REFRESH_TOKEN = os.environ['REFRESH_TOKEN']
+    BASE64_SECRET = b64encode('{}:{}'.format(CLIENT_ID, CLIENT_SECRET).encode()).decode()
+    # URLs
+    API_TOKEN_URL = 'https://accounts.spotify.com/api/token'
