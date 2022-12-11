e068ea128322e3a87f7b904d81df0ec988ab008d
Adrijan Parmakovski <adrijan.parmakovski@gmail.com>
2022-12-11 19:29:28 +0100

    initialisse Flask

--- app/main.py
+++ app/main.py
@@ -1,14 +1,9 @@
-from spotify_crud.client import SpotifyClient
+from flask import Flask
+app = Flask(__name__)
 
-import json
 
-
-if __name__ == '__main__':
-    client = SpotifyClient()
-    tracks = client.get_object(url="https://api.spotify.com/v1/artists/0lAWpj5szCSwM4rUMHYmrr")
-
-    # with open('sample.json', 'w') as f:
-    #     json.dump(tracks[:1], f)
-
-    with open('artist_sample.json', 'w') as f:
-        json.dump(tracks, f)
+@app.route('/', methods=['GET'])
+def main():
+    return {
+        'test': 'app'
+    }
