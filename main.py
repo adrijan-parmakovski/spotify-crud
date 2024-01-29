fe271332f8f07441ed5a74a078e968e62874842e
Adrijan Parmakovski <adrijan.parmakovski@oceansapart.com>
2024-01-29 16:59:55 +0100

    main file for testing

--- main.py
+++ main.py
@@ -1,19 +1,56 @@
 from src.utils.configs import SPOTIFY_CONFIGS
-from src.controllers.client import SpotifyClient
 import json
 
+from src.controllers.client import SpotifyClient, SpotifyApiRequests
+from src.controllers.request_processor import AsyncRequestHandler
+from aiohttp import ClientSession
 
-def foo():
-    # print(SPOTIFY_CONFIGS.__dict__)
-    # print(SPOTIFY_CONFIGS.base_64_secret)
-    # client = SpotifyClient()
-    # tracks = client.get_saved_tracks()
-    # print(tracks[0])
-    # with open("tracks.json", "w") as f:
-    # json.dump(tracks, f)
-    with open("tracks.json", "r") as f:
-        tracks = json.load(f)
-    print(tracks[0])
+from asyncio import run
 
 
-foo()
+# def foo():
+# print(SPOTIFY_CONFIGS.__dict__)
+# print(SPOTIFY_CONFIGS.base_64_secret)
+# client = SpotifyClient()
+# tracks = client.get_saved_tracks()
+# print(tracks[0])
+# with open("tracks.json", "w") as f:
+# json.dump(tracks, f)
+# with open("tracks.json", "r") as f:
+# tracks = json.load(f)
+# print(tracks[0])
+
+API = SpotifyApiRequests(
+    base_url=SPOTIFY_CONFIGS.spotify_web_api_url,
+    client_id=SPOTIFY_CONFIGS.client_id,
+    client_secret=SPOTIFY_CONFIGS.client_secret,
+    refresh_token=SPOTIFY_CONFIGS.refresh_token,
+)
+
+
+async def foo():
+    # r_dict = {"method": "GET", "url": "http://google.com"}
+    session = ClientSession()
+    c = AsyncRequestHandler(session)
+    client = SpotifyClient(api_requests=API, request_processor=c)
+    json_data = await client.get_sample_tracks()
+    with open("sample_track.json", "w") as f:
+        json.dump(json_data[0], f, indent=4)
+    await session.close()
+
+
+def test_request_creation():
+    api = SpotifyApiRequests(
+        base_url=SPOTIFY_CONFIGS.spotify_web_api_url,
+        client_id=SPOTIFY_CONFIGS.client_id,
+        client_secret=SPOTIFY_CONFIGS.client_secret,
+        refresh_token=SPOTIFY_CONFIGS.refresh_token,
+    )
+
+    client = SpotifyClient(api_requests=api)
+    request = client._get_access_token()
+    print(json.dumps(request, indent=4))
+
+
+run(foo())
+# test_request_creation()
